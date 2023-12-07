efa0ef4255830d5ba6feb1154ce969e73413c1a6
André de Magalhães <arfddem@gmail.com>
2023-12-07 18:19:24 -0300

    refactor: renaming hello.go to tools.go

--- go/alura's-go-the-language-of-google/tools.go
+++ go/alura's-go-the-language-of-google/tools.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"runtime"
 	"time"
 )
@@ -14,8 +15,10 @@ func main() {
 	// Shorthand variable declaration
 	iterations := 1
 	startup(author, version, iterations)
-	input := readInput()
-	processInput(input)
+	for {
+		input := readInput()
+		processInput(input)
+	}
 }
 
 func startup(author string, version float64, iterations int) {
@@ -31,10 +34,10 @@ func readInput() int {
 	// If you wish, there is also fmt.Scan only that does not need the specification on which type is being expected. TODO: research on fmt.Scan usages advantages and demises
 	//fmt.Scan(&input)
 	fmt.Println("What would you like to do?")
-	fmt.Println("1 - Fetch Current System OS")
-	fmt.Println("2 - Fetch Current System Date and Time")
-	fmt.Println("3 - Fetch Number of CPUs in the System")
-	fmt.Println("4 - Print 'Hello World!'")
+	fmt.Println("* -> 1 - Fetch Current System OS")
+	fmt.Println("* -> 2 - Fetch Current System Date and Time")
+	fmt.Println("* -> 3 - Fetch Number of CPUs in the System")
+	fmt.Println("* -> 4 - Ping endpoint of your choice")
 	fmt.Scan(&input)
 	return input
 }
@@ -46,14 +49,26 @@ func processInput(input int) {
 	var cpuQuantity int = runtime.NumCPU()
 	switch input {
 	case 1:
-		fmt.Println("Current Running System:", systemOs)
+		fmt.Println("* -> Current Running System:", systemOs)
 	case 2:
-		fmt.Println("Current Date:", formattedDate)
+		fmt.Println("* -> Current Date:", formattedDate)
 	case 3:
-		fmt.Println("Quantity of CPUs in the System:", cpuQuantity)
+		fmt.Println("* -> Quantity of CPUs in the System:", cpuQuantity)
 	case 4:
-		fmt.Println("Hello World!")
+		var url string
+		fmt.Println("* -> Insert the endpoint you'd like to ping for: ")
+		fmt.Scan(&url)
+		ping(url)
 	default:
 		fmt.Println("Unexpected Option")
+	}
+}
+
+func ping(url string) {
+	response, error := http.Get(url)
+	if error == nil {
+		fmt.Printf("Response status from %s with status %s.", url, response.Status)
+	} else {
+		fmt.Printf("Request for %s has returned the following error %s", url, error)
 	}
 }
